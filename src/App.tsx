5854d62a1d5feb4abff8b128fd4bd7825ab01310
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-02-04 14:28:40 +0100

    first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,63 @@
+import { useState } from 'react'
 import './App.css'
+import data from "./books.json"
 
 function App() {
+  const booksPerPage = 5
+  const totalPages = Math.ceil(data.library.length / booksPerPage)
+  const [actualPage, setActualPage] = useState(1)
+  const genres = new Set(data.library.map(el => {
+    return el.book.genre
+  }))
+
+  const nextPage = () => {
+    if (actualPage != totalPages) {
+      setActualPage(actualPage + 1)
+    }
+  }
+  const prevPage = () => {
+    if (actualPage > 1) {
+      setActualPage(actualPage - 1)
+    }
+  }
+
 
   return (
-    <main>
-      <h1>Testing</h1>
-    </main>
+    <>
+      <header>
+        <h1>Books List</h1>
+        <h2>x disponible books</h2>
+      </header>
+
+      <main>
+        <aside style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+          <label htmlFor="" >
+            Filter by pages
+            <br />
+            <a onClick={prevPage}>Prev</a>
+            &nbsp;
+            {actualPage + '/' + totalPages}
+            &nbsp;
+            <a onClick={nextPage}>Next</a>
+          </label>
+          <label htmlFor="" >
+            Filter by genre
+            <br />
+            <select name="" id="">
+              {[...genres].map((genre, index) =>
+                <option key={index} value={genre}>
+                  {genre}
+                </option>
+              )}
+            </select>
+          </label>
+        </aside>
+
+        <section>
+          
+        </section>
+      </main>
+    </>
   )
 }
 
---
+++ src/books.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import data from './books.json'
+
+const books = () => {
+  console.log(data.library)
+
+  return (
+    <div>books</div>
+  )
+}
+
+export default books
